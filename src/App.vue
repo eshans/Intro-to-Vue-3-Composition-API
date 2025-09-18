3db35539cfb9f1143ddc478437933c36e0f54396
Eric Shans <eric.shans@gmail.com>
2025-09-18 14:58:42 -0400

    lessons

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const product = ref('Socdddks')
+const description = ref('these are adidas socks')
+</script>
 
 <template>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-info">
-        <h1>Socks</h1>
-      </div>
-    </div>
-  </div>
+	<div class="product-display">
+		<div class="product-container">
+			<div class="product-info">
+				<h1>{{ product }}</h1>
+				<p>{{ description }}</p>
+			</div>
+		</div>
+	</div>
 </template>
