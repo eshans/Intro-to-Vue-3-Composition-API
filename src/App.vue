a34148ce25e798c9876192e94498cc4267a46f25
Eric Shans <eric.shans@gmail.com>
2025-09-18 16:40:44 -0400

    L4

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,24 @@ import socksGreenImage from './assets/images/socks_green.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
-
+const inventory = ref(8)
+const onSale = ref(true)
 </script>
-  
+
 <template>
-  <div class="nav-bar"></div>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
-      </div>
-      <div class="product-info">
-        <h1>{{ product }}</h1>
-      </div>
-    </div>
-  </div>
+	<div class="nav-bar"></div>
+	<div class="product-display">
+		<div class="product-container">
+			<div class="product-image">
+				<img v-bind:src="image" />
+			</div>
+			<div class="product-info">
+				<h1>{{ product }}</h1>
+				<p v-if="inventory > 10">In stock</p>
+				<p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+				<p v-else>Out of stock</p>
+				<p v-if="onSale">on sale!</p>
+			</div>
+		</div>
+	</div>
 </template>
