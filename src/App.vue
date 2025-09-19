ba38bb2777fa347a51a447b4b05ad16b4d0424f4
Eric Shans <eric.shans@gmail.com>
2025-09-19 17:25:02 -0400

    l6

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,59 @@
 <script setup>
 import { ref } from 'vue'
 import socksGreenImage from './assets/images/socks_green.jpeg'
+import socksBlueImage from './assets/images/socks_blue.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
 const inStock = true
-  
+
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
 const variants = ref([
-  { id: 2234, color: 'green' },
-  { id: 2235, color: 'blue' },
+	{ id: 2234, color: 'green', image: socksGreenImage },
+	{ id: 2235, color: 'blue', image: socksBlueImage }
 ])
 
 const cart = ref(0)
+const addToCart = () => {
+	cart.value += 1
+}
+
+const removeFromCart = () => {
+	if (cart.value > 0) {
+		cart.value -= 1
+	}
+}
+
+const updateImage = (variantImage) => {
+	image.value = variantImage
+}
 </script>
-  
+
 <template>
-  <div class="nav-bar"></div>
-  <div class="cart">Cart({{ cart }})</div>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
-      </div>
-      <div class="product-info">
-        <h1>{{ product }}</h1>
-        <p v-if="inStock">In Stock</p>
-        <p v-else>Out of Stock</p>
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-        <div
-          v-for="variant in variants"
-          :key="variant.id"
-        >
-          {{ variant.color }}
-        </div>
-        <button class="button">Add to Cart</button>
-      </div>
-    </div>
-  </div>
+	<div class="nav-bar"></div>
+	<div class="cart">Cart({{ cart }})</div>
+	<div class="product-display">
+		<div class="product-container">
+			<div class="product-image">
+				<img v-bind:src="image" />
+			</div>
+			<div class="product-info">
+				<h1>{{ product }}</h1>
+				<p v-if="inStock">In Stock</p>
+				<p v-else>Out of Stock</p>
+				<ul>
+					<li v-for="detail in details">{{ detail }}</li>
+				</ul>
+				<div
+					v-for="variant in variants"
+					:key="variant.id"
+					@mouseover="updateImage(variant.image)">
+					{{ variant.color }}
+				</div>
+				<button class="button" @click="addToCart">Add to Cart</button>
+				<button class="button" @click="removeFromCart">Remove from Cart</button>
+			</div>
+		</div>
+	</div>
 </template>
