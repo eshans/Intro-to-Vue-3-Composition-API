150de2ad1df472eec549f872ddc5f71a4dbb7018
Eric Shans <eric.shans@gmail.com>
2025-09-19 16:56:01 -0400

    lesson 5

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,34 @@ const product = ref('Socks')
 const image = ref(socksGreenImage)
 const inStock = true
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
+const variants = ref([
+	{ id: 1222, color: 'green' },
+	{ id: 123, color: 'blue' }
+])
+const sizes = ref(['S', 'M', 'L', 'XL'])
 </script>
-  
+
 <template>
-  <div class="nav-bar"></div>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
-      </div>
-      <div class="product-info">
-        <h1>{{ product }}</h1>
-        <p v-if="inStock">In Stock</p>
-        <p v-else>Out of Stock</p>
-      </div>
-    </div>
-  </div>
+	<div class="nav-bar"></div>
+	<div class="product-display">
+		<div class="product-container">
+			<div class="product-image">
+				<img v-bind:src="image" />
+			</div>
+			<div class="product-info">
+				<h1>{{ product }}</h1>
+				<p v-if="inStock">In Stock</p>
+				<p v-else>Out of Stock</p>
+				<ul>
+					<li v-for="detail in details">{{ detail }}</li>
+				</ul>
+				<div v-for="variant in variants" :key="variant.id">
+					{{ variant.color }}
+				</div>
+				<ul>
+					<li v-for="size in sizes">{{ size }}</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </template>
